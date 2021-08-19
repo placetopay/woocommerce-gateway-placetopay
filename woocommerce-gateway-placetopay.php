6905b751045a6e7cb8952bd176df23ea5df6e7e7
Simón Godoy <sgodoy2017@gmail.com>
2021-08-19 12:04:50 -0500

    feat: Update version

--- woocommerce-gateway-placetopay.php
+++ woocommerce-gateway-placetopay.php
@@ -6,13 +6,13 @@
  * Author: Placetopay
  * Author URI: https://www.placetopay.com/
  * Developer: Placetopay
- * Version: 2.18.4
+ * Version: 2.18.5
  *
  * @package PlacetoPay/WC_Gateway_PlacetoPay
  *
  * @author Soporte <soporte@placetopay.com>
  * @copyright (c) 2013-2017 EGM Ingenieria sin fronteras S.A.S.
- * @version 2.18.4
+ * @version 2.18.5
  */
 
 if (!defined('ABSPATH')) {
@@ -66,7 +66,7 @@ function wc_gateway_placetopay()
     }
 
     require_once(__DIR__ . '/vendor/autoload.php');
-    return \PlacetoPay\PaymentMethod\WC_Gateway_PlacetoPay::getInstance('2.18.4', __FILE__);
+    return \PlacetoPay\PaymentMethod\WC_Gateway_PlacetoPay::getInstance('2.18.5', __FILE__);
 }
 
 add_action('plugins_loaded', 'wc_gateway_placetopay', 0);
