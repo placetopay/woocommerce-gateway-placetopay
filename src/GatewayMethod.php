1f07dbed632309782ab17d6f0025953815cdbc71
Fredy Mendivelso <fredy.mendivelso@evertecinc.com>
2022-07-08 16:11:38 -0500

    Updated headers used: add platform source header

--- src/GatewayMethod.php
+++ src/GatewayMethod.php
@@ -1193,7 +1193,7 @@ class GatewayMethod extends WC_Payment_Gateway
     public static function processPendingOrder($orderId, $requestId)
     {
         $gatewayMethod = new self();
-        $gatewayMethod->initPlacetoPay(true);
+        $gatewayMethod->initPlacetoPay();
         $transactionInfo = $gatewayMethod->placetopay->query($requestId);
         $gatewayMethod->returnProcess(['order_id' => $orderId], $transactionInfo, true);
         $gatewayMethod->logger('Processed order with ID = ' . $orderId, 'cron');
@@ -1336,10 +1336,11 @@ class GatewayMethod extends WC_Payment_Gateway
 
     private function getHeaders(): array
     {
-        $domain = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'];
+        $domain = $_SERVER['HTTP_HOST'] ?? ($_SERVER['SERVER_NAME'] ?? 'localhost');
 
         return [
-            'User-Agent' => "woocommerce-gateway-placetopay/{$this->version} - $domain",
+            'User-Agent' => "woocommerce-gateway-placetopay/{$this->version} (origin:$domain; vr:" . WOOCOMMERCE_VERSION. ')',
+            'X-Source-Platform' => 'woocommerce',
         ];
     }
 
@@ -1347,17 +1348,14 @@ class GatewayMethod extends WC_Payment_Gateway
      * Instantiates a PlacetoPay object providing the login and tranKey,
      * also the url that will be used for the service
      */
-    private function initPlacetoPay(bool $isCallback = false)
+    private function initPlacetoPay()
     {
         $settings = [
             'login' => $this->login,
             'tranKey' => $this->tran_key,
             'baseUrl' => $this->uri_service,
+            'headers' => $this->getHeaders(),
         ];
-
-        if ($isCallback) {
-            $settings['headers'] = $this->getHeaders();
-        }
 
         try {
             $this->placetopay = new PlacetoPay($settings);
