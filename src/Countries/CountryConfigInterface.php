ca095e7021f7b3ff76ea6fa26907657f834740ec
alejociro <66333830+alejociro@users.noreply.github.com>
2023-12-20 15:59:11 -0500

    Feature/pmo 3576 hide fields with default values by client (#34)
    * feat: add configuration of fields by country and cleint
    
    * refactor: get fields and values
    
    * style: translations
    
    * refactor: values of configuration
    
    * refactor: fields by country
    
    * feat: hide fields with debug global variable
    
    ---------
    
    Co-authored-by: Alejandro-Castrillon-Ciro <alejandro.castrillon@evertecinc.com>

--- src/Countries/CountryConfigInterface.php
+++ src/Countries/CountryConfigInterface.php
@@ -2,9 +2,13 @@
 
 namespace PlacetoPay\PaymentMethod\Countries;
 
+use PlacetoPay\PaymentMethod\GatewayMethod;
+
 interface CountryConfigInterface
 {
     public static function resolve(string $countryCode): bool;
+    public static function getConfiguration(GatewayMethod $gatewayMethod): array;
     public static function getEndpoints(string $client): array;
     public static function getClients(): array;
+    public static function getFields(GatewayMethod $gatewayMethod): array;
 }
