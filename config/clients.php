1b92b900a459ad94d55d2b7ccf74b2a7f7187b1d
Alejandro Castrillon Ciro <alejandro.cciro@udea.edu.co>
2025-12-03 15:19:59 -0500

    feat: add clients

---
+++ config/clients.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * White Label Client List
+ * 
+ * This file contains the list of available clients.
+ * All configuration values (client name, country, endpoints, etc.) are defined
+ * directly in the template files located in config/templates/.
+ */
+
+return [
+    'ecuador',
+    'belice',
+    'getnet',
+    'honduras',
+    'uruguay',
+    'avalpay',
+    'banchile',
+];
+
