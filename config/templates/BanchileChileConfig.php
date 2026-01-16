82a578ed52ca3276fac0add9b05fa7209b6410fc
Freddie Gar <freddie.gar@outlook.com>
2026-01-15 23:11:50 -0500

    feat: update client name for BCL

--- config/templates/BanchileChileConfig.php
+++ config/templates/BanchileChileConfig.php
@@ -7,7 +7,7 @@ use PlacetoPay\PaymentMethod\Constants\Environment;
 abstract class CountryConfig
 {
     public const CLIENT_ID = 'banchile-chile';
-    public const CLIENT = 'Banchile pagos';
+    public const CLIENT = 'Banchile Pagos';
     public const IMAGE = 'https://placetopay-static-prod-bucket.s3.us-east-2.amazonaws.com/banchile/logos/Logotipo_superior.png';
     public const COUNTRY_CODE = 'CL';
     public const COUNTRY_NAME = 'Chile';
