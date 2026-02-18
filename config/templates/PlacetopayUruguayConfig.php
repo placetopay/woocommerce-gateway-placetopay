7507b0e47632b5b3202215c7dd66ed23f1953ed0
Alejandro Castrillon Ciro <alejandro.cciro@udea.edu.co>
2026-02-18 15:10:05 -0500

    refactor: remove uy test environment

--- config/templates/PlacetopayUruguayConfig.php
+++ config/templates/PlacetopayUruguayConfig.php
@@ -16,7 +16,6 @@ abstract class CountryConfig
     public static function getEndpoints(): array
     {
         return [
-            Environment::TEST => 'https://uy-uat-checkout.placetopay.com',
             Environment::UAT => 'https://uy-uat.checkout.placetopay.com',
             Environment::PROD => 'https://checkout.placetopay.uy',
         ];
